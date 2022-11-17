cf2fd1f2c82af342e702de59795bbb5ff47e287b
collins <kolichitutu110@gmail.com>
2022-11-17 06:18:07 -0800

    pay

--- server/server.js
+++ server/server.js
@@ -24,6 +24,24 @@ app.get("/api/config/paypal", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
+//PAYMENT
+// In an Express-like app:
+
+app.post("/flw-webhook", (req, res) => {
+  // If you specified a secret hash, check for the signature
+  const secretHash = process.env.FLW_SECRET_HASH;
+  const signature = req.headers["verif-hash"];
+  if (!signature || signature !== secretHash) {
+    // This request isn't from Flutterwave; discard
+    res.status(401).end();
+  }
+  const payload = req.body;
+  // It's a good idea to log all received events.
+  log(payload);
+  // Do something (that doesn't take too long) with the payload
+  res.status(200).end();
+});
+
 const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, console.log(`server run in port ${PORT}`));
